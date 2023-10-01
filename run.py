ffc1f625373cd83f28c65824a380e051e90e740a
AnnNguy3n <nguyenhuuan.k64.hust@gmail.com>
2023-10-02 04:16:17 +0700

    .

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 import CONFIG
-# if CONFIG.METHOD == 1:
-#     from Methods.method_1 import Generator
+if CONFIG.METHOD == 1:
+    from Methods.method_1 import Generator
 
 data, connection, list_get_data_func, list_db_field = CONFIG.check_config()
-print(list_get_data_func, list_db_field)
+
+generator = Generator(data, connection, CONFIG.INTEREST, list_get_data_func, list_db_field)
+try:
+    getattr(generator, CONFIG.MODE.lower())()
+except:
+    generator.save()
+
+connection.close()
